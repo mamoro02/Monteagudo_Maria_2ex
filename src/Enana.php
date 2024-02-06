b2a1127e69686131ffb59146714ffacca63991c6
mery <mamoro02@floridaoberta.es>
2024-02-06 11:37:37 +0100

    prueba 1

--- src/Enana.php
+++ src/Enana.php
@@ -26,7 +26,7 @@ class Enana
         }
     }
 
-    public function heridaLeve()
+    public function heridaLeve( )
     {
         #Se le quitan 10 puntos de vida a la Enana y además se cambia el valor de situacion (si fuera necesario)
         $this->puntosVida -= 10;
